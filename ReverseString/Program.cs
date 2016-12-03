765ca55578b724082e10a71cbb339a1c952e034b
LilcompT <jit_mak@hotmail.co.uk>
2016-12-03 17:44:14 +0000

    Add files via upload

---
+++ ReverseString/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sample = "this";
+            char temp = ' ';
+            int i = 0, j;
+    
+            char[] convert = sample.ToCharArray();
+            
+            if(convert.Length % 2 == 0)
+            {
+                for(i = 0; i < convert.Length/2; i++)
+                {
+                    for(j = convert.Length/2; j < convert.Length; j++)
+                    {
+                            temp = convert[j];
+                            convert[j] = convert[i];
+                            convert[i] = temp;
+                    }
+                }
+            }
+            else
+            {
+                for(i = 0; i < convert.Length/2; i++)
+                {
+                    for(j = (convert.Length/2) + 1; j < convert.Length; j++)
+                    {
+                            temp = convert[j];
+                            convert[j] = convert[i];
+                            convert[i] = temp;
+                    }
+                }
+            }
+
+            sample = new string(convert);
+            Console.WriteLine(sample);
+        }
+    }
+}
+
+//this
+//siht
+
+//homer
+//remoh

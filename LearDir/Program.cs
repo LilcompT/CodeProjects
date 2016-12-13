2ea34889c1fbcda762c566462893a1f06c062b07
LilcompT <jit_mak@hotmail.co.uk>
2016-12-13 15:05:22 +0000

    LearnDir Added

---
+++ LearDir/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace InterfaceExampleApplication
+{
+    public class Vehicle
+    {
+        public static void Main(string[] args)
+        {
+            VehicleWork veh = new VehicleWork();
+            veh.SetName = "bob";
+            veh.EngineType();
+            
+        }
+    }
+
+    interface ITest
+    {
+        string SetName
+        {
+            set; get;
+        }
+        void EngineType();
+    }
+
+    class VehicleWork : ITest
+    {
+        string name = "";
+
+        public string SetName
+        {
+            set{ name = value; }
+            get{ return name; }
+        }
+
+        public void EngineType()
+        {
+            Console.WriteLine("Inheriting Engine via Interface");
+            Console.WriteLine(SetName);
+        }
+    }
+}

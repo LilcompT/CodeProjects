b88c82852f6b67ba981ab2b9c7d00a0427b69a4b
LilcompT <jit_mak@hotmail.co.uk>
2016-12-14 21:40:24 +0000

    Login mech initiated

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 
-namespace ConsoleApplication
+namespace SocialNetworkExperiment
 {
-    public class Program
+    public class SocialNetworkMain
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Variables Declaration
+            bool userSession = false;
+
+            // Object Creation
+            SocialNetworkMain sn = new SocialNetworkMain();
+            
+            if(sn.SigninMech() == true)
+            {
+                userSession = true;
+            }
+
+            while(userSession == true)
+            {
+                // Display User Feed
+            }
         }
+
+        private bool SigninMech()
+        {
+            string userName = "bob";
+            string input = "";
+            int attempt;
+
+            Console.Write("Enter Username: ");
+            input = Console.ReadLine();
+
+            for(attempt = 2; attempt != 0; attempt--)
+            {
+                if(input == userName)
+                {
+                    Console.WriteLine("Login Successful");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect, you have " + attempt + " remaining");
+                    Console.Write("Enter Username: ");
+                    input = Console.ReadLine();    
+                }
+            }
+
+            return false;
+        }
+    }
+    public class UserFeedback
+    {
+
     }
 }

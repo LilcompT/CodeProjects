af8a249ec9fbe3a2dda9f035f6e9b7e680f6c184
LilcompT <jit_mak@hotmail.co.uk>
2016-12-15 17:18:36 +0000

    Forum sub-project added

---
+++ Forum/ForumSource.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForumApplication
+{
+    public class ForumMain
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<Topics> listOfTopics = new LinkedList<Topics>();
+            
+            listOfTopics.AddFirst(new Topics()
+            {
+                TopicTitle = "First Topic",
+                MessageContent = "Test of first message!",
+            });
+
+            listOfTopics.AddLast(new Topics()
+            {
+                TopicTitle = "Topic 2",
+                MessageContent = "Test of second message",
+            });
+
+            foreach(Topics t in listOfTopics)
+            {
+                t.PrintTopic();
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class Topics
+    {
+        public string TopicTitle{get; set;}
+
+        public string MessageContent{get; set;}
+
+        public void PrintTopic()
+        {
+            Console.WriteLine(TopicTitle);
+            Console.WriteLine(MessageContent);
+        }
+    }
+}

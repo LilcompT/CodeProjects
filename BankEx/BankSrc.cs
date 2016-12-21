0def77fcc39d7626d97a529b874e39b5b9cada2b
LilcompT <jit_mak@hotmail.co.uk>
2016-12-21 00:45:28 +0000

    Bank source/abstract class added

---
+++ BankEx/BankSrc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class BankCore
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public abstract class Account
+    {
+        // Widthrawal Method
+        public abstract void Withdraw(double cashAmt);
+        // Deposit Method
+        public abstract void Deposit(double cashAmt);
+        // Loan Method
+        public abstract void Loan(double cashAmt);
+    }
+
+    public class CurrentAcc : Account
+    {
+        
+    }
+}

4f6285eb34771127c8249dd7f87785bc8c983576
LilcompT <jit_mak@hotmail.co.uk>
2017-01-24 01:20:35 +0000

    Index issue fixed, testing required

--- BankEx/BankSrc.cs
+++ BankEx/BankSrc.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
+    ///<summary>
+    /// BankCore class contains methods which will enable user Authentication and the creation of new users.
+    /// The class also operates the flow of the program in terms of how the user account can be created 
+    /// and what operations are available to the user once they are authenticated into the bank system.
+    ///</summary>
     public class BankCore
     {
-
+        // List contains all the bank accounts, which take UserAccountManagement class as type
         private static List<UserAccountManagement> accountList = new List<UserAccountManagement>();
+        // global userID to act a session ID which will act as a index when accessing the accountList
         private static int userID = 0;
+        private enum LoginInput {userLogin = 1, userAdd, userExit};
+        private enum AccountOptions {accountView = 1, accountDeposit, accountWidthraw, accountLogout};  
 
         public static void Main(string[] args)
         {
@@ -17,14 +25,25 @@ namespace ConsoleApplication
             Console.WriteLine("\n---------------------------------");
             Console.WriteLine("WELCOME TO THE BANK OF LEARNING");
             Console.WriteLine("---------------------------------");
+            
+            // Dummy accounts
+            accountList.Add(new UserAccountManagement("bob", "12", 1));
+            accountList.Add(new UserAccountManagement("james", "34", 2));
 
+            // Initiates the Bank front-end
             Bc.BankFront();
         }
-        
+
+        ///<summary>
+        /// BankFront Method is responsible for acting like a user interface, which will allow
+        //  the user pick options regardings the operations that are currently available.
+        ///</summary>
         private void BankFront()
         {
-            
+            // Holds user input
             int optionInput = 0;
+            // Will contain the last index of the List, to be used when progressing from AddNewUser method.
+            int lastIndex = 0;
         
             Console.WriteLine();
             Console.WriteLine("Please select an option");
@@ -35,6 +54,10 @@ namespace ConsoleApplication
             Console.Write("Option: ");
             optionInput = Int32.Parse(Console.ReadLine());
             
+            /*  Option 1 will execute the Authentication mechinism, where if the condition is met
+                then the user will be proceeded to the bank front-end.
+                Else if the condition fails then then Login failure message will be displayed  
+            */
             if(optionInput == 1)
             {
                 if(Authentication() == true)
@@ -46,18 +69,29 @@ namespace ConsoleApplication
                     Console.WriteLine("Login Failed");
                 }
             }
+            /*
+                If Option 2 is inputted then AddNewUser method will be executed, enabling the insertion
+                of new users.
+            */
             else if(optionInput == 2)
             {
+                // Executes the AddNewUser enabling user account creationa and insertion into the accountList
                 AddNewUser();
-                DisplayMainInterface(userID);
+                // Retrives the last index of the accountList to enforce progression of the user account/program flow
+                lastIndex = accountList.Count - 1;
+                // Displays the user interface where the user case choose account operations
+                DisplayMainInterface(lastIndex);
             }
             else if(optionInput ==  3)
             {
                 //System exit
             }
-
         }
-
+        ///<summary>
+        /// Method AddNewUser enables the creation of new user bank accounts, where its account, name, password
+        /// and account type can be defined. Consequentially the user account is directed added into the accountList 
+        /// upon the retrival of user input.
+        ///</summary>
         private void AddNewUser()
         {
             string userName = "";
@@ -73,9 +107,15 @@ namespace ConsoleApplication
             Console.Write("\nOption: ");
             accountType = Int32.Parse(Console.ReadLine());
 
+            // Creates a new UserAccountManagement object with the parameters of the user account info
             accountList.Add(new UserAccountManagement(userName, passcode, accountType));
         }
 
+        ///<summary>
+        /// Method Authentication processes the log-in mechinism for the user account based on the
+        /// information provided. The method returns a boolean which can be used to determine whether
+        /// if the user credentials match the present credentials in the accountList.
+        ///</summary>
         public bool Authentication()
         {
            string loginName = "";
@@ -89,26 +129,36 @@ namespace ConsoleApplication
            Console.WriteLine();
            Console.Write("Enter passcode: ");
            loginPass = Console.ReadLine();
-        
-       
+           
+           // While loop to control the amount of attempts user can make
            while(attempt != 1)
            {
+               // Traverses the accountList to seek the appropriate information, if exsisting
                foreach(UserAccountManagement user in accountList)
                {
                    if(loginName == user.UserName && loginPass == user.UserPasscode)
-                   {
-                       // Problem Area
-                       // Implement id into UserAccountManagement to act as index
+                   {    
+                       // sets up the account index which will allocate the correct corrosponding account
                        userID = index;
+                       Console.WriteLine("List index: " + userID);
+                       // credentials are matched so return true
                        return true;
                    }
 
+                   // Updates the index whilst Traversing the accountList
                    ++index;
                }
+               // Updates the amount of attempts made by the users
                attempt++;
            }
+           // Returns false if no corrosponding account matches are found based on the input provided
            return false;
         }
+
+        ///<summary>
+        /// Method DisplayMainInterface is responsible for presenting the account operates which the user
+        /// can make on their account.
+        ///</summary>
         private void DisplayMainInterface(int userID)
         {
             int OptionInput = 0;
@@ -127,8 +177,7 @@ namespace ConsoleApplication
                 switch(OptionInput)
                 {
                     case 1:
-                        accountList[userID].ViewAccount(++ID);
-                        Console.WriteLine("User ID: " + ID);
+                        accountList[userID].ViewAccount();
                         taskNotFinished = false;
                         DisplayMainInterface(userID);
                         break;
@@ -158,7 +207,7 @@ namespace ConsoleApplication
                         }
                         else if(accountList[userID].accountType == savingAccType)
                         {
-                            accountList[userID].Withdraw(cashInput, currentAccType);
+                            accountList[userID].Withdraw(cashInput, savingAccType);
                         }
 
                         taskNotFinished = false;
@@ -173,7 +222,6 @@ namespace ConsoleApplication
             }
         }
     }
-
     public abstract class Account
     {
         // Widthrawal Method
@@ -181,9 +229,8 @@ namespace ConsoleApplication
         // Deposit Method
         public abstract void Deposit(double cashAmt, int accountType);
 
-        public abstract void ViewAccount(int accountType);
+        public abstract void ViewAccount();
     }
-
     class UserAccountManagement : Account
     {
        private  string name;
@@ -214,7 +261,6 @@ namespace ConsoleApplication
                savingBalance += cashAmt;
            }
        }
-
        public override void Withdraw(double cashAmt, int accountType)
        {
            if(accountType == 1)
@@ -226,14 +272,13 @@ namespace ConsoleApplication
                savingBalance -= cashAmt;
            }
        }
-
-       public override void ViewAccount(int inputAccount)
+       public override void ViewAccount()
        {   
-           if(inputAccount == accType)
+           if(accType == 1)
            {
                Console.WriteLine("Name: {0} Account Type: {1} Balanace Remaining: {2}", name, accType, currentBalance);
            }
-           else if(inputAccount == accType)
+           else if(accType == 2)
            {
                Console.WriteLine("Name: {0} Account Type: {1} Balanace Remaining: {2}", name, accType, savingBalance);
            }
@@ -245,7 +290,6 @@ namespace ConsoleApplication
                return name;
            }
        }
-
        public string UserPasscode
        {
            get
@@ -253,7 +297,6 @@ namespace ConsoleApplication
                return passcode;
            }
        }
-
        public int accountType
        {
            get
